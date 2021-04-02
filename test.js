5b02523c1bbd4f7cdcdd30eb8d7be0cb01679961
imaguiraga <imaguiraga@users.noreply.github.com>
2021-04-02 01:43:29 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+define(["require", "exports"], function (require, exports) {
+  "use strict";
+  Object.defineProperty(exports, "__esModule", { value: true });
+  exports.test = void 0;
+  let x = 6;
+  function test() {
+      return 'hello';
+  }
+  exports.test = test;
+});
